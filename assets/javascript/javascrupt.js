cdb0e6b066d6a45463342ba610cf9e7d5fdfd1a4
Brendan Gutleber <gutleberb@gmail.com>
2019-03-02 19:01:33 -0600

    added the capture of the search term when search is clicked

---
+++ assets/javascript/javascrupt.js
@@ -0,0 +1,20 @@
+
+// store api key
+var apiKey = "0IjhpI7O7SDDQhlVmi1iK2Nik0fTeg58"
+
+
+// waits for page to load
+$(document).ready(function () {
+
+
+
+    // event for on the search button click
+    $("#button-addon2").on("click", function () {
+
+        var searchTerm = $("#search-term").val();
+
+        console.log(searchTerm)
+
+    });
+
+});
